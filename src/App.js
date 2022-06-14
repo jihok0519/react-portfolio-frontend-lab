ef139bf0c797807a3967fd1b9d827a57227e603a
Enoch Kim <jihok0519@gmail.com>
2022-06-14 14:30:08 -0400

    frontend complete

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
+
+function App() {
+  const URL = "http://localhost:4000/";
+
+  return (
+    <div className="App">
+      <Header />
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/projects">
+        <Projects URL={URL} />
+      </Route>
+      <Route path="/about">
+        <About URL={URL} />
+      </Route>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,5 @@
+const Home = (props) => {
+    return <h1>Welcome to the Home Page</h1>
+};
+
+export default Home;
